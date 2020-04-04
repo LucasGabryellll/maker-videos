2c6e889f23d351a99d2019d8cc6bf762208f222a
Lucas Gabryel <LucasGabryellll>
2020-04-04 01:39:20 -0300

    feat : add search term and prefix

---
+++ index.js
@@ -0,0 +1,27 @@
+const readLine = require('readline-sync');
+
+function start() {
+    const content = {};
+
+    content.searchTerm = askAndReturnSearchTerm()//Função para pegar o input do user(Termo para pesquisa)
+    content.prefix = askAndReturnPrefix()//(procurar por prefixo)
+                                                
+
+    function askAndReturnSearchTerm() {
+        return readLine.question('Digite Um Termo de Pesquisa da Wikipedia: ');//retorna a String no objeto
+    };
+    
+    function askAndReturnPrefix() {
+        //retorna o valor do Text e colocar no content.prefix
+        const prefixes = ['Quem e', 'O que e', 'A historia de'];
+        const selectedPrefixIndex = readLine.keyInSelect(prefixes);//pega o index do array
+        const selectedPrefixText = prefixes[selectedPrefixIndex];//Ler o que tem no index selecionado do array
+
+        return selectedPrefixText;
+    };
+
+    console.log(content);
+
+};
+
+start()
